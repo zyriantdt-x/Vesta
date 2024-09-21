12597b68487beaebc91a1f02223344762b97584c
Ellis <me@ellis.pw>
2024-09-21 20:23:08 +0100

    create packet handler structure for go_to_flat

---
+++ Vesta.Server/Packets/Incoming/Flats/GoToFlatPacketHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vesta.Server.Sessions;
+using Vesta.Server.Streams;
+
+namespace Vesta.Server.Packets.Incoming.Flats;
+internal class GoToFlatPacketHandler : IPacketHandler {
+    public short Header => IncomingPackets.GO_TO_FLAT;
+
+    public async Task Handle( ISession session, IRequest request ) {
+    
+    }
+}
